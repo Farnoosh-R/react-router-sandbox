ca12bd88b2e545d8a1032c3e29c7721cbcb6538b
farnoosh <daarcparsa@yahoo.com>
2024-07-07 14:04:08 +0330

    rout without refresh-Link

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Posts from "./pages/Posts";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/posts" element={<Posts />} />
